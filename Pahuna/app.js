5950b3fae71f4578e04a06b059012e60d9d030ac
Supreme <supremesuwal9@gmail.com>
2026-08-16 16:25:47 +0545

    Added Review Router

--- Pahuna/app.js
+++ Pahuna/app.js
@@ -9,6 +9,7 @@ const asyncWrap=require('./utils/asyncWrap')
 const {listingSchema, reviewSchema}=require('./schema.js')
 const Review=require('./models/Review.js')
 const listings=require('./routes/listings.js')
+const reviews=require('./routes/reviews.js')
 
 const app = express()
 
@@ -55,25 +56,9 @@ const validateReview=(req,res,next)=>{
 }
 
 app.use('/listings',listings)
+app.use('/listings/:id/reviews',reviews)
 
-// review add route
-app.post('/listings/:id/reviews',validateReview,asyncWrap(async(req,res,next)=>{
-    let {id}=req.params
-    let listing=await Listing.findById(id).populate('review')
-    const newReview=new Review(req.body.review)
-    listing.review.push(newReview)
-    await newReview.save()
-    await listing.save()
-    res.redirect(`/listings/${id}`)
-}))
 
-//review delete route
-app.delete('/listings/:id/reviews/:reviewId',async(req,res)=>{
-    let {id,reviewId}=req.params
-    await Listing.findByIdAndUpdate(id,{$pull:{review:reviewId}})
-    await Review.findByIdAndDelete(reviewId)
-    res.redirect(`/listings/${id}`)
-})
 
 //check route
 app.use((req, res, next) => {
---
+++ Pahuna/routes/reviews.js
@@ -0,0 +1,39 @@
+const express=require('express')
+const router=express.Router({mergeParams:true})
+const Listing = require('../models/listings')
+const Review=require('../models/Review')
+const asyncWrap=require('../utils/asyncWrap')
+const ExpressError = require('../utils/ExpressError')
+const {listingSchema, reviewSchema}=require('../schema')
+
+const validateReview=(req,res,next)=>{
+   let {error}= reviewSchema.validate(req.body)
+
+   if(error){
+    throw new ExpressError(400,error)
+   }
+   else{
+    next()
+   }
+}
+
+// review add route
+router.post('/',validateReview,asyncWrap(async(req,res,next)=>{
+    let {id}=req.params
+    let listing=await Listing.findById(id).populate('review')
+    const newReview=new Review(req.body.review)
+    listing.review.push(newReview)
+    await newReview.save()
+    await listing.save()
+    res.redirect(`/listings/${id}`)
+}))
+
+//review delete route
+router.delete('/:reviewId',async(req,res)=>{
+    let {id,reviewId}=req.params
+    await Listing.findByIdAndUpdate(id,{$pull:{review:reviewId}})
+    await Review.findByIdAndDelete(reviewId)
+    res.redirect(`/listings/${id}`)
+})
+
+module.exports=router
